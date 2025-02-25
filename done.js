fbbeaeef557dccec84433800ed5c72f3a8f3a88e
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-02-25 15:43:13 +0000

    done

--- done.js
+++ done.js
@@ -50,3 +50,21 @@ alert("You want " + scoops + " scoops of " + flavor + " ice cream" );
 
 
 /* Prompt with Conditional */
+/* Var Alert Prompt Conditional */
+// make a string variable for color, prompt the user for their favorite
+var color = prompt("What is your favorite color?");
+// if user enters "black" alert "Black is not really a color."
+if ( color == "black") alert("Black is not really a color.");
+// otherwise alert color " is a nice color!"var color = prompt("What your favorite color?")
+else alert(color + " is a nice color! " );
+
+
+/* Var Alert Prompt Conditional */
+// make an integer variable for age, prompt user for it
+var age = prompt("How old are you?");
+// make a string variable for name, prompt user for it
+var name1 = prompt("What is your name?");
+// if age is greater than 16, alert "you don't look that old!"
+if ( age > 16 ) alert( name1 +  " don't look that old!");
+// otherwise alert name is age years old
+else alert( name1 + " is " + age + " years old!");
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* Var Alert Prompt Conditional */
+// make an integer variable for age, prompt user for it
 var age = prompt("How old are you?");
-var name1 = prompt("Your name?");
-alert(name1 + "is" + age+" years old ");
+// make a string variable for name, prompt user for it
+var name1 = prompt("What is your name?");
+// if age is greater than 16, alert "you don't look that old!"
+if ( age > 16 ) alert( name1 +  " doesn't look that old!");
+// otherwise alert name is age years old
+else alert( name1 + " is " + age + " years old!");

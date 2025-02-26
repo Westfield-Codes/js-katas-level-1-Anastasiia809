6c1f3ea01ba9922852003afc85452b6d14e943f9
Anastasiia809 <anastasiia208805@student-schoolsofwestfield.org>
2025-02-26 18:55:01 +0000

    almost done

--- done.js
+++ done.js
@@ -61,6 +61,20 @@ alert("You want " + scoops + " scoops of " + flavor + " ice cream" );
 
 
 /* Prompt */
+/* Var Alert Prompt Conditionals */
+// make a variable for pet type, prompt user for it 
+var petType = prompt("What type of pets do you like? ");
+// make a variable for pet name, prompt user for it
+var petName = prompt("How do you call your pet?");
+// alert "You have a pet type named pet name 
+alert("You have " + petType + " named " + petName);
+// if pet is a dog, say "I like dogs, too!"
+if ( petType=="dog") alert("I like dogs too!");
+// if it is a cat, say "I'm allergic to cats"
+if (petType=="cat") alert("I'm allergic to cats!");
+// If it is not a dog or a cat, say "what an interesting pet!"
+else alert("What an intresting pet!");
+
 
 
 /* Prompt with Conditional */
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 /* Var Alert Prompt Conditionals */
 // make a variable for pet type, prompt user for it 
-var petType = prompt("What type of pets do you like? ")
+var petType = prompt("What type of pets do you like? ");
 // make a variable for pet name, prompt user for it
+var petName = prompt("How do you call your pet?");
 // alert "You have a pet type named pet name 
+alert("You have " + petType + " named " + petName);
 // if pet is a dog, say "I like dogs, too!"
+if ( petType=="dog") alert("I like dogs too!");
 // if it is a cat, say "I'm allergic to cats"
+if (petType=="cat") alert("I'm allergic to cats!");
 // If it is not a dog or a cat, say "what an interesting pet!"
